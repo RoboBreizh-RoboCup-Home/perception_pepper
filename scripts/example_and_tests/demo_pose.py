13b2506c66a1b94df20a6d64d30e3c525643ca4d
maelic <teoneau@gmail.com>
2023-10-17 09:41:59 +0200

    First ros2 test

--- scripts/example_and_tests/demo_pose.py
+++ scripts/example_and_tests/demo_pose.py
@@ -3,8 +3,15 @@
 
 from .PoseDetection.MoveNet_MultiPose.movenet_multipose import MoveNetMultiPose
 
+import Camera.Naoqi_camera as nc
+from Camera.naoqi_camera_types import CameraID, CameraResolution2D as res2D, CameraResolution3D as res3D, ColorSpace2D as cs2D, ColorSpace3D as cs3D
 
 class PoseDemo(Node):
     def __init__(self):
         super().__init__('PoseDemo')
         self.pose_model = MoveNetMultiPose(pose_model_name="movenet_multipose")
+        depth_camera_res = res3D.R320x240
+        rgb_camera_res = res2D.R320x240
+    
+        cameras = nc.NaoqiCameras(ip=qi_ip, resolution = [rgb_camera_res, depth_camera_res])
+        
